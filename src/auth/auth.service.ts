b5777475e7615d3db8aabf815b38afa2baa84519
firas haidar <haidar_firas013@hotmail.com>
2025-10-11 18:36:10 +0300

    oauth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -440,6 +440,8 @@ export class AuthService {
       user: {
         id: user.id,
         email: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName,
         role: user.role,
         status: user.status,
       },
